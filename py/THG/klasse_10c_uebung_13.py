9f252abc86aae74ca959037d707e24246d04089a
kassbohm <kassbohm@gmail.com>
2020-07-17 14:57:58 +0200

    "tag"

--- py/THG/klasse_10c_uebung_13.py
+++
@@ -1,1 +0,0 @@
-/home/kai/A_Sphinx/Teach/thg/source/10c/10c_Uebungen/Uebung_13/klasse_10c_uebung_13.py
--- py/THG/lambacher_schweizer_9_page_92_task_8_cc.py
+++
@@ -1,1 +0,0 @@
-/home/kai/A_Sphinx/Teach/thg/source/9e/9e_Uebungen/Uebung_09/lambacher_schweizer_9_page_92_task_8_cc.py
